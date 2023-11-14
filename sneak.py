5578e03d30f68cdd7835afb79eaf8be68685a7ad
Matthew Barbara <matthew.barbara@snyk.io>
2023-11-14 12:10:32 +0100

    test

--- sneak.py
+++ sneak.py
@@ -5,8 +5,6 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
    # file Copyright.txt or https://cmake.org/licensing for details.
 import argparse
-import codecs
-import copy
 import logging
 import json
 import os
